2630f75c78bf9af39ad2655727778ec95cc929a9
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-18 16:01:58 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'home/login'
-=======
   get 'nutritionlogs/nutrition'
   get 'workoutlogs/workouts'
   get 'posts/posts'
   get 'medals/medals'
->>>>>>> tmp
-  #Log in page root/get
   root "articles#login"
-  #get "/articles", to: "articles#login"
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
