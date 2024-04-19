0be5d874b5089b5ae4262b957d71a11d69244fb2
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-18 21:15:40 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'posts/posts'
   get 'medals/medals'
   root to: "articles#login"
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
