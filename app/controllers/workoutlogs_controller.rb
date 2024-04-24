fc5627f04997c0e32fdf7ac198aea6463ff0c8d5
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-24 11:50:51 -0400

    Update workoutlogs_controller.rb

--- app/controllers/workoutlogs_controller.rb
+++ app/controllers/workoutlogs_controller.rb
@@ -8,4 +8,18 @@ class WorkoutlogsController < ApplicationController
   def show
     @workoutlog = Workoutlog.find(params[:id])
   end
+
+  def new
+    @workoutlog = Workoutlog.new
+  end
+
+  def create
+    @workoutlog = Workoutlog.new(day: "...", type: "...", description: "...")
+
+    if @workoutlog.save
+      redirect_to @workoutlog
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
