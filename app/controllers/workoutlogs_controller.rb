a088981211c17136d4122c6822d165b13b59b4e5
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-24 11:39:59 -0400

    Update workoutlogs_controller.rb

--- app/controllers/workoutlogs_controller.rb
+++ app/controllers/workoutlogs_controller.rb
@@ -1,6 +1,11 @@
 class WorkoutlogsController < ApplicationController
   before_action :authenticate_user!
+  
   def workouts
     @workoutlogs = Workoutlog.all
+  end
+
+  def show
+    @workoutlog = Workoutlog.find(params[:id])
   end
 end
