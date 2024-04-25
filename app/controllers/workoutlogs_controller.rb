792d969f44f7d1b320b40280a64b502d98b15a19
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-25 08:52:11 -0400

    Update workoutlogs_controller.rb

--- app/controllers/workoutlogs_controller.rb
+++ app/controllers/workoutlogs_controller.rb
@@ -14,7 +14,7 @@ class WorkoutlogsController < ApplicationController
   end
 
   def create
-    @workoutlog = Workoutlog.new(day: "...", regimen: "...", description: "...")
+    @workoutlog = Workoutlog.new(workoutlog_params)
 
     if @workoutlog.save
       redirect_to @workoutlog
