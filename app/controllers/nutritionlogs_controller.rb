09856c6ddbb7920705dc2f0154b3cff2f1e43cfa
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-25 15:34:34 -0400

    Update nutritionlogs_controller.rb

--- app/controllers/nutritionlogs_controller.rb
+++ app/controllers/nutritionlogs_controller.rb
@@ -5,16 +5,16 @@ class NutritionlogsController < ApplicationController
 
   def add_calories
     @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.calories_consumed += 100 # Or any other value you prefer
+    @nutrition_log.calories_consumed += 100
     @nutrition_log.save
     redirect_to @nutrition_log
   end
 
   def remove_calories
     @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.calories_consumed -= 100 # Or any other value you prefer
+    @nutrition_log.calories_consumed -= 100
     @nutrition_log.save
     redirect_to @nutrition_log
   end
-
+  
 end
