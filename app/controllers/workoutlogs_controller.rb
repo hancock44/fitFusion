af943042e1c4e7f8c6c0a1b5acce662656d83bd3
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-25 08:39:12 -0400

    Update workoutlogs_controller.rb

--- app/controllers/workoutlogs_controller.rb
+++ app/controllers/workoutlogs_controller.rb
@@ -1,6 +1,6 @@
 class WorkoutlogsController < ApplicationController
   before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token
+  
   def index
     @workoutlogs = Workoutlog.all
   end
