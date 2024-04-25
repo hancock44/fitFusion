8c154bad254686d6d643799bb96f767a1bcce3b3
Drake006 <157472450+Drake006@users.noreply.github.com>
2024-04-25 15:38:55 -0400

    Update routes.rb user specific view

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   #Workout Routes
   root "workoutlogs#index"
   
-  resources :workoutlogs
+  resources :workoutlogs, only: :index
   
   #Posts Routes
   get 'posts/posts'
