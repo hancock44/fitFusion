b92ee79c69e9f77f1480b6fe15947b3d2553801c
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-25 08:47:30 -0400

    Update workoutlog.rb

--- app/models/workoutlog.rb
+++ app/models/workoutlog.rb
@@ -3,5 +3,5 @@ class Workoutlog < ApplicationRecord
 
   validates :day, presence: true
   validates :regimen, presence: true
-  validates :description, presence: true, length: { minimum: 10 }
+  validates :description, presence: true, length: { minimum: 1 }
 end
