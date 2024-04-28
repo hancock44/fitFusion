1abfcb89782660756999c156db1763b315ba7a1b
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-28 18:56:56 -0400

    Update nutritionlogs_controller.rb

--- app/controllers/nutritionlogs_controller.rb
+++ app/controllers/nutritionlogs_controller.rb
@@ -41,43 +41,31 @@ class NutritionlogsController < ApplicationController
   end
   
   def add_calories
-    @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.calories_consumed += 100
-    @nutrition_log.save
-    redirect_to @nutrition_log
+    modify_nutrition_log(:calories_consumed, 100)
   end
 
   def remove_calories
-    @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.calories_consumed -= 100
-    @nutrition_log.save
-    redirect_to @nutrition_log
+    modify_nutrition_log(:calories_consumed, -100)
   end
 
   def add_protein
-    @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.protein_intake += 5
-    @nutrition_log.save
-    redirect_to @nutrition_log
+    modify_nutrition_log(:protein_intake, 5)
   end
 
   def remove_protein
-    @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.protein_intake -= 5
-    @nutrition_log.save
-    redirect_to @nutrition_log
+    modify_nutrition_log(:protein_intake, -5)
   end
 
   def add_sleep
-    @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.current_sleep_hours += 5
-    @nutrition_log.save
-    redirect_to @nutrition_log
+    modify_nutrition_log(:current_sleep_hours, 5)
   end
 
   def remove_sleep
-    @nutrition_log = NutritionLog.find(params[:id])
-    @nutrition_log.current_sleep_hours -= 5
+    modify_nutrition_log(:current_sleep_hours, -5)
+  end
+
+  def modify_nutrition_log(attribute, value)
+    @nutrition_log[attribute] += value
     @nutrition_log.save
     redirect_to @nutrition_log
   end
