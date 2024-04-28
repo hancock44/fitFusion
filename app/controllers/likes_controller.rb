c0b6b09f7fd16e0872af80a48d6169c41b7c300d
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-28 19:48:17 -0400

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,6 +6,16 @@ class LikesController < ApplicationController
     @post.likes.create(user_id: current_user.id)
     redirect_to post_path(@post)
   end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
+    redirect_to post_path(@post)
+  end
+  
   private
   def find_post
     @post = Post.find(params[:post_id])
