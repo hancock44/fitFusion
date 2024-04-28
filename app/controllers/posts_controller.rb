8046772bccf8f2a2bf3529b3d3cf7bbcb39f912d
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-28 19:52:07 -0400

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+  before_action :find_like, only: [:destroy]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
