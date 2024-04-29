4105b64e754b483e5b78eb1797cdb00a4a3b2e3c
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-28 20:36:37 -0400

    Update nutrition_log.rb

--- app/models/nutrition_log.rb
+++ app/models/nutrition_log.rb
@@ -1,7 +1,3 @@
 class NutritionLog < ApplicationRecord
   belongs_to :user
-
-  validates :max_calories, presence: true
-  validates :min_protein, presence: true
-  validates :min_sleep, presence: true
 end
