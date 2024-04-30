661a225fc40b14adf13c5eb0614fafdfce83a6ff
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-30 10:49:36 -0400

    Update workoutlog.rb

--- app/models/workoutlog.rb
+++ app/models/workoutlog.rb
@@ -1,3 +1,7 @@
 class Workoutlog < ApplicationRecord
   belongs_to :user
+
+  validates :day, length: { maximum: 255 }
+  validates :regimen, length: { maximum: 255 }
+  validates :description, length: { maximum: 1000 }
 end
