83eb8e78df33fc0d81f938181c517394a4761d92
Ubuntu <ubuntu@ip-172-31-17-234.us-east-2.compute.internal>
2024-04-30 19:37:15 +0000

    removal of workoutlogs

--- test/controllers/workoutlogs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class WorkoutlogsControllerTest < ActionDispatch::IntegrationTest
-  test "should get workouts" do
-    get workoutlogs_workouts_url
-    assert_response :success
-  end
-end
--- test/models/workoutlog_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class WorkoutlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
