eef827aff9ccf7ca2d46ffde95a574a88dc5626a
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-04-30 15:44:06 -0400

    Update workout_log.rb

--- app/models/workout_log.rb
+++ app/models/workout_log.rb
@@ -1,3 +1,10 @@
 class WorkoutLog < ApplicationRecord
   belongs_to :user
+  before_validation :set_default_day, on: :create
+
+  private
+
+  def set_default_day
+    self.day ||= Date.today
+  end
 end
