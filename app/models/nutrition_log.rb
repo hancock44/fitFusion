21253856d6a8214cfcb839ef85c28e038acc0a3e
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-02 15:06:52 -0400

    Update nutrition_log.rb

--- app/models/nutrition_log.rb
+++ app/models/nutrition_log.rb
@@ -1,3 +1,11 @@
 class NutritionLog < ApplicationRecord
   belongs_to :user
+
+  before_validation :set_default_day, on: :create
+
+  private
+
+  def set_default_day
+    self.day ||= Date.today
+  end
 end
