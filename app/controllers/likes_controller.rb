8ec9e893d4e5985bec07e4b41b8eb4c1f7599404
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-02 14:42:23 -0400

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -24,7 +24,11 @@ class LikesController < ApplicationController
   def find_post
     @post = Post.find(params[:post_id])
   end
-
+  
+  def find_like
+    @like = @post.likes.find(params[:id])
+  end
+  
   def already_liked?
       Like.where(user_id: current_user.id, post_id:
       params[:post_id]).exists?
