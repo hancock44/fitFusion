440166b102198fe667ebafef33bec68e40f5e76a
Drake006 <157472450+Drake006@users.noreply.github.com>
2024-05-05 19:42:16 -0400

    Update workout_log.rb private status

--- app/models/workout_log.rb
+++ app/models/workout_log.rb
@@ -1,7 +1,10 @@
 class WorkoutLog < ApplicationRecord
   belongs_to :user
   before_validation :set_default_day, on: :create
+  
+  VALID_STATUSES = ['private']
 
+  validates :status, inclusion: { in: VALID_STATUSES }
   private
 
   def set_default_day
