3bccc73b7f2402b4ef93969f649f823ab36c806a
Ubuntu <ubuntu@ip-172-31-17-234.us-east-2.compute.internal>
2024-05-05 23:33:39 +0000

    add status to workout

---
+++ db/migrate/20240505232807_add_status_to_workout_logs.rb
@@ -0,0 +1,5 @@
+class AddStatusToWorkoutLogs < ActiveRecord::Migration[7.1]
+  def change
+    add_column :workout_logs, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_185708) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_232807) do
   create_table "likes", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_185708) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["user_id"], name: "index_workout_logs_on_user_id"
   end
 
