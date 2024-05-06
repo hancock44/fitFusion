6eb92d169ffc9b3f810b627641ae4f917c2f6464
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-06 19:59:37 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :nutrition_logs
-  has_many :workoutlogs
+  has_many :workout_logs
   has_many :posts
   has_many :likes, dependent: :destroy
 end
