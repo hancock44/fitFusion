06ea334887c00171802587f525eca33c918df22a
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-06 19:57:26 -0400

    try showing only current user

--- app/controllers/workout_logs_controller.rb
+++ app/controllers/workout_logs_controller.rb
@@ -4,7 +4,7 @@ class WorkoutLogsController < ApplicationController
 
   # GET /workout_logs or /workout_logs.json
   def index
-    @workout_logs = WorkoutLog.all
+    @workout_logs = current_user.workout_logs.where(user_id: current_user.id)
   end
 
   # GET /workout_logs/1 or /workout_logs/1.json
