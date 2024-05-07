4b6f8bd174a48fbcf6810e7dee583516e6ee6636
Ubuntu <ubuntu@ip-172-31-17-234.us-east-2.compute.internal>
2024-05-07 01:17:29 +0000

    remove migration

--- db/migrate/20240507011211_add_workout_logs_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddWorkoutLogsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :workout_logs_count, :integer
-    add_column :users, :default, :string
-    add_column :users, :0, :string
-  end
-end
