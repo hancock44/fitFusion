badd3ba6999c09dea9be2264b04c0ca63adcfb5f
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-06 21:44:19 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessible :workout_logs_count
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
