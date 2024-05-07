c648c11933d4e2ea49c6282fa062222d7494634d
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-06 21:35:44 -0400

    Update constants.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,11 @@
-LEVEL_MESSAGES = {
+WARRIORLEVEL_MESSAGES = {
   bronze: "You've reached the bronze level!",
   silver: "You've reached the silver level!",
   gold: "Congratulations! You've reached the gold level!"
 }.freeze
 
+NERDLEVEL_MESSAGES = {
+  bronze: "You've reached the bronze level!",
+  silver: "You've reached the silver level!",
+  gold: "Congratulations! You've reached the gold level!"
+}.freeze
