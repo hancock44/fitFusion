8ca5f386929f4d31510a7780eb780e0701cd5ff8
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-05-07 20:40:35 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_003446) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer :workout_logs_count, :default => 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
